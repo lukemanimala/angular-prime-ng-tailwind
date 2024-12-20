791cea0ab6f582d8da6f605b1f0b33c62aebd1ad
lmanimalaAAR <96892680+lmanimalaAAR@users.noreply.github.com>
2024-12-20 15:48:35 -0600

    Initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+// Import prime components
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { DatePicker } from 'primeng/datepicker';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [
+    FormsModule,
+    // prime components
+    InputTextModule,
+    FloatLabelModule,
+    ButtonModule,
+    DatePicker,
+  
+  ]
 })
+
+
 export class AppComponent {
-  title = 'MyPrimeNGApp';
+  title = 'MyPrimeNGApp'; // App title
+  value3: string = ''; // Two-way binding variable for input text
+  dateValue: Date | null = null; // Two-way binding variable for calendar
 }
